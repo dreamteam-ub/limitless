85b1f11c5f74d16e8dcb73433fde58f7971e9697
manuMatnez <mmartima285@alumnes.ub.edu>
2019-03-14 20:27:59 +0100

    cambios en los tamaños de imagenes

--- app/src/main/java/edu/ub/pis/joc/limitless/MenuActivity.kt
+++ app/src/main/java/edu/ub/pis/joc/limitless/MenuActivity.kt
@@ -11,22 +11,21 @@ class MenuActivity : ActivityNoTopBar() {
         setContentView(R.layout.activity_menu)
 
         val playButton : ImageButton = findViewById(R.id.play_btn)
-        val rankButton : ImageButton = findViewById(R.id.rankings_btn)
-        val optButton : ImageButton = findViewById(R.id.option_btn)
-        val infoButton : ImageButton = findViewById(R.id.info_btn)
-
         playButton.setOnClickListener {
             Toast.makeText(this,"PLAY",Toast.LENGTH_SHORT).show()
         }
 
+        val rankButton : ImageButton = findViewById(R.id.rankings_btn)
         rankButton.setOnClickListener {
             Toast.makeText(this,"RANKINGS",Toast.LENGTH_SHORT).show()
         }
 
+        val optButton : ImageButton = findViewById(R.id.option_btn)
         optButton.setOnClickListener {
             Toast.makeText(this,"OPTIONS",Toast.LENGTH_SHORT).show()
         }
 
+        val infoButton : ImageButton = findViewById(R.id.info_btn)
         infoButton.setOnClickListener {
             Toast.makeText(this,"INFO",Toast.LENGTH_SHORT).show()
         }
