cc8ea444b2dae26266d16e30771f719b3ffce434
arnau <greadercf9@gmail.com>
2019-03-18 21:29:18 +0100

    Petit canvi per amagar la barra de navegacio a dispositius que la tinguin a pantalla.

--- app/src/main/java/edu/ub/pis/joc/limitless/ActivityNoTopBar.kt
+++ app/src/main/java/edu/ub/pis/joc/limitless/ActivityNoTopBar.kt
@@ -3,6 +3,7 @@ package edu.ub.pis.joc.limitless
 import android.annotation.SuppressLint
 import android.os.Bundle
 import android.support.v7.app.AppCompatActivity
+import android.view.View
 import android.view.Window
 import android.view.WindowManager
 
@@ -13,5 +14,22 @@ open class ActivityNoTopBar : AppCompatActivity() {
         //* Hides Notification Bar
         window.requestFeature(Window.FEATURE_NO_TITLE)
         window.setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN, WindowManager.LayoutParams.FLAG_FULLSCREEN)
+
+        window.decorView.systemUiVisibility = View.SYSTEM_UI_FLAG_HIDE_NAVIGATION
+
+    }
+
+
+    override fun onWindowFocusChanged(hasFocus:Boolean) {
+        super.onWindowFocusChanged(hasFocus);
+        if (hasFocus) {
+            window.decorView.systemUiVisibility =
+                    View.SYSTEM_UI_FLAG_LAYOUT_STABLE or
+                    View.SYSTEM_UI_FLAG_LAYOUT_HIDE_NAVIGATION or
+                    View.SYSTEM_UI_FLAG_LAYOUT_FULLSCREEN or
+                    View.SYSTEM_UI_FLAG_HIDE_NAVIGATION or
+                    View.SYSTEM_UI_FLAG_FULLSCREEN or
+                    View.SYSTEM_UI_FLAG_IMMERSIVE_STICKY;
+        }
     }
 }
--- app/src/main/java/edu/ub/pis/joc/limitless/LoginActivity.kt
+++ app/src/main/java/edu/ub/pis/joc/limitless/LoginActivity.kt
@@ -3,6 +3,7 @@ package edu.ub.pis.joc.limitless
 import android.content.Intent
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
 
 import com.google.android.gms.common.SignInButton
 
