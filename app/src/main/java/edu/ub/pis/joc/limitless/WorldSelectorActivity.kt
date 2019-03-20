54eadf559fb6a2010ee731ad47e4075d4c22a046
aaronPeruga <perugaaron99@gmail.com>
2019-03-20 15:02:14 +0100

    Activity de selector de mundos hecha

---
+++ app/src/main/java/edu/ub/pis/joc/limitless/WorldSelectorActivity.kt
@@ -0,0 +1,12 @@
+package edu.ub.pis.joc.limitless
+
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+
+class WorldSelectorActivity : ActivityNoTopBar() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_world_selector)
+    }
+}
