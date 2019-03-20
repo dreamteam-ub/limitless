85bd4ec95edc4968ac45b854ed4d1b11d6014f99
arnau <greadercf9@gmail.com>
2019-03-20 23:04:23 +0100

    Canvi de logo i ajust de la navegació entre mons y nivells

--- app/src/main/java/edu/ub/pis/joc/limitless/WorldSelectorActivity.kt
+++ app/src/main/java/edu/ub/pis/joc/limitless/WorldSelectorActivity.kt
@@ -27,79 +27,104 @@ class WorldSelectorActivity : ActivityNoTopBar() {
                                          R.drawable.world3_select,
                                          R.drawable.world4_select)
 
-        //TextViews
+        //Variables
         val worldTitle: TextView = findViewById(R.id.world_one_title)
         val lvlTitle: TextView = findViewById(R.id.level_title)
-
-
         val arrowBack: ImageButton = findViewById(R.id.world_back_button)
+        val worldPhoto: ImageButton = findViewById(R.id.world_one)
+        val leftArrow: ImageButton = findViewById(R.id.change_left_arrow)
+        val rightArrow: ImageButton = findViewById(R.id.change_right_arrow)
+        val downArrow: ImageButton = findViewById(R.id.change_down_arrow)
+        val upArrow: ImageButton = findViewById(R.id.change_up_arrow)
+
+        upArrow.visibility = View.INVISIBLE
+        leftArrow.visibility = View.INVISIBLE
+
         arrowBack.setOnClickListener {
             finish()
         }
 
-        val worldPhoto: ImageButton = findViewById(R.id.world_one)
         worldPhoto.setOnClickListener {
             val myMapGameInt = Intent(this, GameActivity::class.java)
             myMapGameInt.putExtra("mode","My Map")
             startActivity(myMapGameInt)
         }
 
-
-        val leftArrow: ImageButton = findViewById(R.id.change_left_arrow)
         leftArrow.setOnClickListener {
             LVL -= 1
-            if(LVL < 0){LVL = 2}
             when(LVL) {
-                0 -> lvlTitle.text = resources.getString(R.string.first_level)
-                1 -> lvlTitle.text = resources.getString(R.string.second_level)
-                2 -> lvlTitle.text = resources.getString(R.string.third_level)
+                0 -> {
+                    lvlTitle.text = resources.getString(R.string.first_level)
+                    leftArrow.visibility = View.INVISIBLE
+                }
+                1 -> {
+                    lvlTitle.text = resources.getString(R.string.second_level)
+                    rightArrow.visibility = View.VISIBLE
+                }
             }
 
         }
 
-        val rightArrow: ImageButton = findViewById(R.id.change_right_arrow)
         rightArrow.setOnClickListener {
             LVL += 1
-            LVL = LVL% nLevels
             when(LVL){
-                0-> lvlTitle.text = resources.getString(R.string.first_level)
-                1-> lvlTitle.text = resources.getString(R.string.second_level)
-                2-> lvlTitle.text = resources.getString(R.string.third_level)
+                1-> {
+                    lvlTitle.text = resources.getString(R.string.second_level)
+                    leftArrow.visibility = View.VISIBLE
+                }
+                2->{
+                    lvlTitle.text = resources.getString(R.string.third_level)
+                    rightArrow.visibility = View.INVISIBLE
+                }
             }
 
         }
 
-
-
-        val downArrow: ImageButton = findViewById(R.id.change_down_arrow)
         downArrow.setOnClickListener {
-            World -= 1
-            if(World < 0){World = 3}
+            LVL = 0
+            lvlTitle.text = resources.getString(R.string.first_level)
+            leftArrow.visibility = View.INVISIBLE
+            rightArrow.visibility = View.VISIBLE
+
+            World += 1
             worldPhoto.setImageResource(img[World])
             when(World){
-                0-> worldTitle.text = resources.getString(R.string.world_one_title)
-                1-> worldTitle.text = resources.getString(R.string.world_two_title)
-                2-> worldTitle.text = resources.getString(R.string.world_three_title)
-                3-> worldTitle.text = resources.getString(R.string.world_four_title)
+                1-> {
+                    worldTitle.text = resources.getString(R.string.world_two_title)
+                    upArrow.visibility = View.VISIBLE
+                }
+                2-> {
+                    worldTitle.text = resources.getString(R.string.world_three_title)
+                }
+                3-> {
+                    worldTitle.text = resources.getString(R.string.world_four_title)
+                    downArrow.visibility = View.INVISIBLE
+                }
             }
         }
 
-        val upArrow: ImageButton = findViewById(R.id.change_up_arrow)
         upArrow.setOnClickListener {
             //SETEAMOS EL Nivel
             LVL = 0
             lvlTitle.text = resources.getString(R.string.first_level)
+            leftArrow.visibility = View.INVISIBLE
+            rightArrow.visibility = View.VISIBLE
 
-            World += 1
-            World = World % nWorlds
+            World -= 1
             worldPhoto.setImageResource(img[World])
             when(World){
-                0-> worldTitle.text = resources.getString(R.string.world_one_title)
-                1-> worldTitle.text = resources.getString(R.string.world_two_title)
-                2-> worldTitle.text = resources.getString(R.string.world_three_title)
-                3-> worldTitle.text = resources.getString(R.string.world_four_title)
+                0-> {
+                    worldTitle.text = resources.getString(R.string.world_one_title)
+                    upArrow.visibility = View.INVISIBLE
+                }
+                1-> {
+                    worldTitle.text = resources.getString(R.string.world_two_title)
+                }
+                2-> {
+                    worldTitle.text = resources.getString(R.string.world_three_title)
+                    downArrow.visibility = View.VISIBLE
+                }
             }
-
         }
     }
 
