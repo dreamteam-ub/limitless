5a1925bca9ebbf3e46f2ce0464e069abc0fa3a7b
paubernabe <pbernaco7@alumnes.ub.edu>
2019-03-29 13:58:40 +0100

    singleton y multithreading añadido a Presenter

--- app/src/main/java/edu/ub/pis/joc/limitless/model/Level.kt
+++ app/src/main/java/edu/ub/pis/joc/limitless/model/Level.kt
@@ -1,8 +1,8 @@
 package edu.ub.pis.joc.limitless.model
 
+import edu.ub.pis.joc.limitless.presenter.Presenter
+
 class Level {
 
     var name : String? = null
-
-
 }
--- app/src/main/java/edu/ub/pis/joc/limitless/presenter/Presenter.kt
+++ app/src/main/java/edu/ub/pis/joc/limitless/presenter/Presenter.kt
@@ -1,6 +1,18 @@
 package edu.ub.pis.joc.limitless.presenter
 
+
+
+
 class Presenter {
+
+
+    companion object {
+        val instance : Presenter by lazy(LazyThreadSafetyMode.PUBLICATION){ Presenter()}
+
+    }
+    fun getInstance(): Presenter {
+        return instance
+    }
 
 
 }
