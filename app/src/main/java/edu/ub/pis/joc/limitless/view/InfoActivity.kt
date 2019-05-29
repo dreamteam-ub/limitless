72f9f8995bc5ba26573e16db7ee21e4c11d8db25
manuMatnez <mmartima285@alumnes.ub.edu>
2019-05-29 19:06:20 +0200

    info intents

--- app/src/main/java/edu/ub/pis/joc/limitless/view/InfoActivity.kt
+++ app/src/main/java/edu/ub/pis/joc/limitless/view/InfoActivity.kt
@@ -1,5 +1,7 @@
 package edu.ub.pis.joc.limitless.view
 
+import android.content.Intent
+import android.net.Uri
 import android.os.Bundle
 import android.widget.ImageButton
 import android.widget.TextView
@@ -31,10 +33,37 @@ class InfoActivity : FullScreenActivity() {
         ViewAdjuster.adjustView(findViewById(R.id.info_developers))
         ViewAdjuster.adjustView(findViewById(R.id.info_music_rights))
 
-        ViewAdjuster.adjustView(findViewById(R.id.developers_details1))
-        ViewAdjuster.adjustView(findViewById(R.id.developers_details2))
-        ViewAdjuster.adjustView(findViewById(R.id.developers_details3))
-        ViewAdjuster.adjustView(findViewById(R.id.developers_details4))
+        val dev1 : TextView = findViewById(R.id.developers_details1)
+        ViewAdjuster.adjustView(dev1)
+
+        dev1.setOnClickListener {
+            val github = Intent(Intent.ACTION_VIEW, Uri.parse(getString(R.string.detail_github_developer1)))
+            startActivity(github)
+        }
+
+        val dev2 : TextView = findViewById(R.id.developers_details2)
+        ViewAdjuster.adjustView(dev2)
+
+        dev2.setOnClickListener {
+            val github = Intent(Intent.ACTION_VIEW, Uri.parse(getString(R.string.detail_github_developer2)))
+            startActivity(github)
+        }
+
+        val dev3 : TextView = findViewById(R.id.developers_details3)
+        ViewAdjuster.adjustView(dev3)
+
+        dev3.setOnClickListener {
+            val github = Intent(Intent.ACTION_VIEW, Uri.parse(getString(R.string.detail_github_developer3)))
+            startActivity(github)
+        }
+
+        val dev4 : TextView = findViewById(R.id.developers_details4)
+        ViewAdjuster.adjustView(dev4)
+
+        dev4.setOnClickListener {
+            val github = Intent(Intent.ACTION_VIEW, Uri.parse(getString(R.string.detail_github_developer4)))
+            startActivity(github)
+        }
 
         ViewAdjuster.adjustView(findViewById(R.id.music_rights_details))
     }
